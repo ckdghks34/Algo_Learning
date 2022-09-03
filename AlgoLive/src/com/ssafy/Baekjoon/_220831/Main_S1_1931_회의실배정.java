c1296667d88f08c9892cf64dbaf3241506af15e5
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-09-03 22:17:24 +0900

    [220831]_[Greedy]_[Baekjoon_1931_회의실배정]

---
+++ AlgoLive/src/com/ssafy/Baekjoon/_220831/Main_S1_1931_회의실배정.java
@@ -0,0 +1,62 @@
+package com.ssafy.Baekjoon._220831;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main_S1_1931_회의실배정 {
+    static class Meeting implements Comparable<Meeting>{
+        int startTime;
+        int endTime;
+
+        Meeting(int startTime, int endTime){
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public int compareTo(Meeting m){
+            if(this.endTime == m.endTime)
+                return this.startTime - m.startTime;
+
+            return this.endTime - m.endTime;
+        }
+
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int count = 0;
+
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Meeting> list = new ArrayList<>();
+
+        for(int i =0 ;i < n;++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            int startTime = Integer.parseInt(st.nextToken());
+            int endTime = Integer.parseInt(st.nextToken());
+
+            list.add(new Meeting(startTime,endTime));
+        }
+        Collections.sort(list);
+
+
+        int curTime = 0;
+        for(int i = 0; i < n;++i)
+        {
+            Meeting meeting = list.get(i);
+            if(meeting.startTime >= curTime) {
+                count++;
+                curTime = list.get(i).endTime;
+            }
+        }
+
+        bw.write(Integer.toString(count));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
